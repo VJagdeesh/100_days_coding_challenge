b0e67b273cf59c3c4c5c04d813051a615d2895f9
unknown <jagdeeshbhavani22@gmail.com>
2024-10-13 00:13:37 +0530

    Search in Rotated Sorted Array

---
+++ Day-47/SearchInRotatedSortedArray.java
@@ -0,0 +1,33 @@
+// 33. Search in Rotated Sorted Array
+
+class SearchInRotatedSortedArray {
+    public static int search(int[] nums, int target) {
+        int l = 0;
+        int h = nums.length - 1;
+        while (l <= h) {
+            int mid = (l + h) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] >= nums[l]) {
+                if (nums[l] <= target && target <= nums[mid]) {
+                    h = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[h]) {
+                    l = mid + 1;
+                } else {
+                    h = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 4, 5, 6, 7, 0 };
+        int target = 0;
+        System.out.println(search(nums, target)); // Output: 4
+    }
+}
